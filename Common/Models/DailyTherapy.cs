36a064416ddccbe13100267b243b44ef52c7826c
Stefan Vasic <stefan_94@rocketmail.com>
2020-06-20 12:57:46 +0200

    Common - added models

---
+++ Common/Models/DailyTherapy.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+
+namespace MyTherapyWPF.Common
+{
+	//[Serializable]
+	public class DailyTherapy : INotifyPropertyChanged
+	{
+		private double dose;
+		private DateTime date;
+
+		[JsonProperty(PropertyName = "id")]
+		public int Id { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets daily dosage of therapy.
+		/// </summary>
+		[JsonProperty(PropertyName="dose")]
+		public double Dose
+		{
+			get => dose;
+			set
+			{
+				dose = value;
+				OnPropertyChanged("Dose");
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets date for daily therapy.
+		/// </summary>
+		[JsonProperty(PropertyName = "date")]
+		public DateTime Date {
+			get => date;
+			set
+			{
+				date = value;
+				OnPropertyChanged("Date");
+			} 
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
